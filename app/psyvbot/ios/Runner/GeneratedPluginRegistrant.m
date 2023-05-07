f95c1030bf2f5dc62e5a2391408da9e3f8fbeac2
Thaveesha Dheerasekera <thaveesha.00@gmail.com>
2023-05-07 12:59:29 +0530

    ready to deploy

---
+++ app/psyvbot/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_tts/FlutterTtsPlugin.h>)
+#import <flutter_tts/FlutterTtsPlugin.h>
+#else
+@import flutter_tts;
+#endif
+
+#if __has_include(<path_provider_foundation/PathProviderPlugin.h>)
+#import <path_provider_foundation/PathProviderPlugin.h>
+#else
+@import path_provider_foundation;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterTtsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterTtsPlugin"]];
+  [PathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"PathProviderPlugin"]];
+}
+
+@end
---
+++ server/prediction.py
@@ -0,0 +1,41 @@
+import json
+from flask import jsonify
+import os
+import math
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import logging
+logging.getLogger('tensorflow').setLevel(logging.ERROR)
+
+from keras.models import load_model
+from keras.preprocessing.text import Tokenizer
+from keras.utils import pad_sequences
+from keras.models import load_model
+
+# Load the saved model from the .h5 file
+model = load_model('/Users/thaveesha/Projects/PsyVBot/models/final_models/lstm_e5_b32_es.h5')
+
+def predict_depression(user_inputs):
+    # Load the tokenizer that you used during training
+    tokenizer = Tokenizer(num_words=5000)
+    tokenizer.fit_on_texts(user_inputs)
+
+    # Convert the input strings into sequences of integers
+    sequences = tokenizer.texts_to_sequences(user_inputs)
+
+    # Pad the sequences to a fixed length
+    padded_sequences = pad_sequences(sequences, maxlen=100)
+
+    # Make a prediction on the padded sequences
+    predictions = model.predict(padded_sequences)
+
+    prediction_avg = sum(predictions)/len(user_inputs)
+
+    # Convert the ndarray to a list
+    prediction_list = prediction_avg.tolist()
+
+    # data = {"prediction": math.trunc(prediction_list[0])}
+    data = {"prediction": float(f'{prediction_list[0]:.5f}')}
+
+    # Return the prediction as a JSON-serializable string
+    return jsonify(data)
